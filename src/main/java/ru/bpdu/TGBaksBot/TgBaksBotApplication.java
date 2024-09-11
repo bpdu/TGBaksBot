c63d756eafc00cc8978a9a7f0a4f69abde291e50
Alexey Rumyantsev <dev@bpdu.ru>
2024-09-11 11:21:02 +0300

    Create a project structure with Spring Initializr

---
+++ src/main/java/ru/bpdu/TGBaksBot/TgBaksBotApplication.java
@@ -0,0 +1,13 @@
+package ru.bpdu.TGBaksBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBaksBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBaksBotApplication.class, args);
+	}
+
+}
