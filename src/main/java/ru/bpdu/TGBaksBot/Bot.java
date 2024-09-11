41beca4e1f70012b3ce356fa94cb94c4cb808629
Alexey Rumyantsev <dev@bpdu.ru>
2024-09-11 13:29:58 +0300

    Implement TelegramLongPollingBot methods

---
+++ src/main/java/ru/bpdu/TGBaksBot/Bot.java
@@ -0,0 +1,26 @@
+package ru.bpdu.TGBaksBot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Bot extends TelegramLongPollingBot{
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println(update);    
+    }
+
+    @Override
+    public String getBotToken() {
+        Dotenv dotenv = Dotenv.load();
+        return dotenv.get("API_TOKEN");
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "TGBaksBot";
+    }
+    
+}
