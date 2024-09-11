0d4dea6a4c3cae0e3c7d6847c32803eb41bb8f27
Alexey Rumyantsev <dev@bpdu.ru>
2024-09-11 13:43:15 +0300

    create a bot

--- src/main/java/ru/bpdu/TGBaksBot/TgBaksBotApplication.java
+++ src/main/java/ru/bpdu/TGBaksBot/TgBaksBotApplication.java
@@ -2,12 +2,16 @@ package ru.bpdu.TGBaksBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class TgBaksBotApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws TelegramApiException {
+		TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+  		botsApi.registerBot(new Bot());
 		SpringApplication.run(TgBaksBotApplication.class, args);
 	}
-
 }
